570a24b960c5521741a24f9796e5eecada9bef80
Mehran Shahidi <m.mehrashahidi@gmail.com>
2022-01-19 11:04:06 +0330

    ✨ signUp page with minimal styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form'
 
-
-function App() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data:any) => console.log(data);
-  return (
-  <div dir="rtl" className="max-w-2xl border-2 mx-auto">
-    <h1 className="text-xl font-sans">فرم </h1> 
-    <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)} >
-      <input {...register("firstName")} />
-      <select {...register("gender")}>
-         <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
-      <input type="sumbit" />
-    </form>
-  </div>
-  );
+const elemText = {
+  eng: {
+    signUp: 'Sign up',
+    email: 'Email',
+    name: 'Name',
+    password: 'Password',
+    passwordConf: 'Confirm Password',
+    havAccount: 'Already have an account?',
+    requiredMsg: 'This is required',
+  },
+  ir: {
+    signUp: 'ثبت نام',
+    email: 'پست الکترونیک',
+    name: 'نام',
+    password: 'کلمه عبور',
+    passwordConf: 'تایید کلمه عبور',
+    haveAccount: 'قبلا ثبت نام کردم؟',
+    requiredMsg: 'این فیلد لازم است',
+  },
 }
 
-export default App;
+function App() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = (data: any) => console.log(data)
+  return (
+    <div
+      dir="rtl"
+      className="bg-background-800 max-w-lg border-2 mx-auto w-3/4"
+    >
+      <div className="flex flex-col items-center text-center mt-16 p-2">
+        <h1 className="text-xl font-bold text-main-50">
+          {elemText['ir'].signUp}{' '}
+        </h1>
+        <form
+          className="w-full flex flex-col space-y-2 mt-3"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input
+            placeholder={elemText['ir'].name}
+            {...register('firstName', {
+              required: elemText['ir'].requiredMsg,
+            })}
+          />
+          <input placeholder={elemText['ir'].email} />
+          <input type='password' placeholder={elemText['ir'].password} />
+          <input type='password' placeholder={elemText['ir'].passwordConf} />
+          <button
+            className="bg-primary-500 rounded-3xl p-2 text-main-50 font-bold"
+            type="submit"
+          >
+            {elemText['ir'].signUp}
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
+  darkMode: 'class',
   theme: {
     fontFamily: {
       sans: ['Vazir', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.teal,
+        secondary: colors.gray,
+        accent: colors.rose,
+        main: colors.coolGray,
+        background: colors.blueGray
+
+      }
+    },
   },
   plugins: [
     require('tailwindcss-rtl'),
